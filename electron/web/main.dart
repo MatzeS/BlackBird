1a5de0ff9fa85c4210128fc3e765882bb3bfeaaa
Matthias <matthias.sokolowski@googlemail.com>
2018-12-19 19:02:37 +0100

    Initialized empty angular app

--- electron/web/main.dart
+++ electron/web/main.dart
@@ -3,8 +3,18 @@ import 'package:blackbird_electron/interop.dart';
 import 'package:node_interop/node.dart' hide require;
 import 'package:node_interop/path.dart';
 
+import 'dart:html';
+import 'package:node_interop/node.dart' hide require;
+import 'package:node_interop/path.dart';
+import 'package:js/js.dart';
+import 'package:angular/angular.dart';
+import 'package:blackbird_electron/app_component.template.dart' as ng;
+
 void main() {
+  runApp(ng.AppComponentNgFactory);
+
   var p = require<Path>('path');
-  window..console.info(p.join(process.cwd(), 'foo', 'bar'));
+  console.log('test');
+  // window..console.info(p.join(process.cwd(), 'foo', 'bar'));
   // ..alert('Hello, Dart! Electron version: ${process.version}');
 }
